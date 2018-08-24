5f0c09dc94be1a504d27d315d517c1d79c923f2f
freek <freek@spatie.be>
2018-08-24 18:40:48 +0200

    formatting

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,4 @@
-Nova.booting((Vue) => {
+Nova.booting(Vue => {
     Vue.component('index-nova-tags-field', require('./components/Nova/IndexField'));
     Vue.component('detail-nova-tags-field', require('./components/Nova/DetailField'));
     Vue.component('form-nova-tags-field', require('./components/Nova/FormField'));
