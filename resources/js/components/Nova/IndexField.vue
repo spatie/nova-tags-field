ad0a6a4c94173c38aed0214b434504c0b0e8518b
Kazi Mainuddin Ahmed <mailtokmahmed@gmail.com>
2019-11-14 00:53:44 +0530

    Consistent styling in Index Page (#68)

--- resources/js/components/Nova/IndexField.vue
+++ resources/js/components/Nova/IndexField.vue
@@ -1,5 +1,5 @@
 <template>
-    <p class="py-3 tags-index-field" v-html="typeof field.value === 'object' ? field.value.join(', ') : field.value"></p>
+    <div class="py-3 tags-index-field" v-html="typeof field.value === 'object' ? field.value.join(', ') : field.value"></div>
 </template>
 
 <script>
