54b011472d9c6384d68ff375be833b27a078c203
maks-zu <zubovmaksim@gmail.com>
2022-05-05 15:16:47 +0300

    recompiled with the latest laravel-nova js source instead of npm package

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,7 @@
 let mix = require('laravel-mix')
+require('./nova.mix')
 
 mix.setPublicPath('dist')
     .js('resources/js/field.js', 'js')
-    .vue({version: 3})
-    .webpackConfig({
-        externals: {
-            vue: 'Vue',
-        },
-        output: {
-            uniqueName: 'spatie/nova-tags-field',
-        },
-        resolve: {
-            symlinks: false
-        }
-    })
+    .vue({version: 3 })
+    .nova('spatie/nova-tags-field');
