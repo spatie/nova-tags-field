1f17db697fd729c872033cf8afca307baf9d19df
Mior Muhammad Zaki <crynobone@gmail.com>
2022-08-25 11:36:05 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/js/components/Nova/DetailField.vue
+++ resources/js/components/Nova/DetailField.vue
@@ -1,13 +1,25 @@
 <template>
     <PanelItem :index="index" :field="field">
         <template #value>
-            <p v-html="typeof field.value === 'object' ? field.value.join(', ') : field.value"></p>
+            <p v-html="tagFieldValue"></p>
         </template>
     </PanelItem>
 </template>
 
 <script>
+import { FieldValue } from 'laravel-nova';
+
 export default {
+    mixins: [FieldValue],
+
     props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+
+    computed: {
+        tagFieldValue() {
+            let fieldValue = this.fieldValue
+
+            return typeof fieldValue === 'object' ? fieldValue.join(', ' : fieldValue
+        },
+    },
 };
 </script>
