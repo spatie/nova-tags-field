0263aea02968a7fa89255724c461439607dd9264
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-02 02:02:13 +0800

    Add workflow to automatically update assets, fixes `displayUsing` and reduce `lodash` dist (#164)
    * Add workflow to automatically update assets.
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Update Assets
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Update Assets
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    Co-authored-by: crynobone <crynobone@users.noreply.github.com>

--- resources/js/components/Nova/IndexField.vue
+++ resources/js/components/Nova/IndexField.vue
@@ -1,12 +1,24 @@
 <template>
     <div
         class="py-3 tags-index-field"
-        v-html="typeof field.value === 'object' ? field.value.join(', ') : field.value"
+        v-html="tagFieldValue"
     ></div>
 </template>
 
 <script>
+import { FieldValue } from 'laravel-nova';
+
 export default {
+    mixins: [FieldValue],
+
     props: ['resourceName', 'field'],
+
+    computed: {
+        tagFieldValue() {
+            let fieldValue = this.fieldValue
+
+            return typeof fieldValue === 'object' ? fieldValue.join(', ') : fieldValue
+        },
+    },
 };
 </script>
